e395b05dba3e3ca2b090c21409424357f7ca2152
ccrock4t <15344554+ccrock4t@users.noreply.github.com>
2023-11-18 21:05:04 -0500

    Upload utility scripts

---
+++ Assets/Scripts/Utils/GlobalUtils.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Reflection;
+using UnityEngine;
+
+public class GlobalUtils
+{
+    public static void LogErrorFeatureNotImplemented(string error_message)
+    {
+        Debug.LogError("Feature not yet implemented:" + error_message);
+    }
+
+    public static void LogErrorEnumNotRecognized(string error_message)
+    {
+        Debug.LogError("Setting not recognized: " + error_message);
+    }
+
+    public static string stringValueOf(Enum value)
+    {
+        FieldInfo fi = value.GetType().GetField(value.ToString());
+        DescriptionAttribute[] attributes = (DescriptionAttribute[])fi.GetCustomAttributes(typeof(DescriptionAttribute), false);
+        if (attributes.Length > 0)
+        {
+            return attributes[0].Description;
+        }
+        else
+        {
+            return value.ToString();
+        }
+    }
+
+    // given the "description" or string value, get the corresponding enum.
+    public static object? enumValueOf(string value, Type enumType)
+    {
+        string[] names = System.Enum.GetNames(enumType);
+        foreach (string name in names)
+        {
+            if (stringValueOf((Enum)Enum.Parse(enumType, name)).Equals(value))
+            {
+                return Enum.Parse(enumType, name);
+            }
+        }
+
+        return null; // not found
+    }
+}
